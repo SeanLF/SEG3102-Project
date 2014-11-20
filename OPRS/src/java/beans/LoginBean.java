ddf61cb5c5979714d089dff9516313b619813989
Sean Floyd <sfloy029@uottawa.ca>
2014-11-19 22:32:16 -0500

    Modify add property page to be user friendly and change ui element code

--- OPRS/src/java/beans/LoginBean.java
+++ OPRS/src/java/beans/LoginBean.java
@@ -89,7 +89,7 @@ public class LoginBean {
                  if (Arrays.equals(checkPassHash, acc.getPassword())) {
                      //login ok - set user in session context
                      HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-                     session.setAttribute("User", acc);
+                     session.setAttribute("UserAccount", acc);
                      status="Login Successful - " + "Welcome " + acc.getFirstname(); 
                  } else {
                     status="Invalid Login, Please Try again"; 
